ee71b89ed24fe82d36dcdefe3b5586ca805fc016
Hooda Armaan <hooda.armaan@syk.fi>
2023-01-31 07:23:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import ibadts.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] input = scanner.nextLine().split(" ");
+        System.out.println(balancedBrackets(input));
+    }
+
+    public static boolean balancedBrackets(String [] input){
+        Stack<String> stack = new Stack<>();
+        for (String x : input) {
+            if (x.equals("(") || x.equals("{") || x.equals("[")) {
+                stack.push(x);
+                continue;
+            }
+            if (stack.isEmpty())
+                return false;
+            String checker;
+            switch (x) {
+                case ")" -> {
+                    checker = stack.pop();
+                    if (checker.equals("{") || checker.equals("["))
+                        return false;
+                }
+                case "}" -> {
+                    checker = stack.pop();
+                    if (checker.equals("(") || checker.equals("["))
+                        return false;
+                }
+                case "]" -> {
+                    checker = stack.pop();
+                    if (checker.equals("(") || checker.equals("{"))
+                        return false;
+                }
+            }
+        }
+        return (stack.isEmpty());
+    }
+}
